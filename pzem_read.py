4ee2125666c72969ce2c8dffd4443ac81f160fa8
foggybottle <czessiu@gmail.com>
2023-02-16 15:44:08 +0100

    initial version

---
+++ pzem_read.py
@@ -0,0 +1,73 @@
+"""
+######################################################################
+
+Simple Modbus Sensor Polling Code
+Coded By "The Intrigued Engineer" over a coffee
+
+Minimal Modbus Library Documentation
+https://minimalmodbus.readthedocs.io/en/stable/
+
+Thanks For Watching!!!
+
+######################################################################
+"""
+
+import minimalmodbus # Don't forget to import the library!!
+
+mb_address = 1 # Modbus address of sensor
+
+sensy_boi = minimalmodbus.Instrument('/dev/ttyACM0',mb_address) # Make an "instrument" object called sensy_boi (port name, slave address (in decimal))
+
+sensy_boi.serial.baudrate = 9600                                # BaudRate
+sensy_boi.serial.bytesize = 8                                   # Number of data bits to be requested
+sensy_boi.serial.parity = minimalmodbus.serial.PARITY_NONE      # Parity Setting here is NONE but can be ODD or EVEN
+sensy_boi.serial.stopbits = 1                                   # Number of stop bits
+sensy_boi.serial.timeout  = 0.5                                 # Timeout time in seconds
+sensy_boi.mode = minimalmodbus.MODE_RTU                         # Mode to be used (RTU or ascii mode)
+
+# Good practice to clean up before and after each execution
+sensy_boi.clear_buffers_before_each_transaction = True
+sensy_boi.close_port_after_each_call = True
+
+## Uncomment this line to print out all the properties of the setup a the begining of the loop
+print(sensy_boi)
+
+print("")
+print("Requesting Data From Sensor...") # Makes it look cool....
+
+# NOTE-- Register addresses are offset from 40001 so inputting register 0 in the code is actually 40001, 3 = 40004 etc...
+
+
+# Get list of values from MULTIPLE registers
+# Arguments - (register start address, number of registers to read, function code)
+data =sensy_boi.read_registers(0, 9, 4)
+
+print("")
+print(f"Raw data is {data}") # Shows the raw data list for the lolz
+
+# Process the raw data by deviding by 10 to get the actual floating point values
+
+voltage = data[0]/10
+current = data[1]/10
+power =  data[3]*10
+energy =  data[5]/10
+frequency = data[7]/10
+powerfactor = data[8]
+
+# Print out the processed data in a little table
+# Pro-tip > \u00B0 is the unicode value for the degree symbol which you can see before the "C" in temperature
+print("-------------------------------------")
+print(f"Voltage = {voltage} V")
+print(f"Current = {current} A")
+print(f"Power = {power} W")
+print(f"Frequency = {frequency} Hz")
+print(f"energy = {energy} Wh")
+print(f"powerfactor = {powerfactor}")
+print("-------------------------------------")
+print("")
+
+# Piece of mind close out
+sensy_boi.serial.close()
+print("Ports Now Closed")
+
+# Now Need to put values in to MQTT.....
